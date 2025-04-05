b03faf0f26e2f7c7a1d9b3c98f7fcf37a0c8da7a
ChillerDragon <ChillerDragon@gmail.com>
2025-04-05 18:49:41 +0200

    Hello world teeworlds
    @Swarfeya cool stuff so far :D

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
-const hello: String = "hello"
-console.log(hello)
+import { Client } from 'teeworlds'
+const client = new Client('127.0.0.1', 8303, 'chillerbot-ts')
+
+client.connect().catch(e => console.log(e))
+
+client.on('connected', () => {
+  console.log('Connected!')
+})
+
+client.on('disconnect', reason => {
+  // you got kicked from the server
+  console.log('Disconnected: ' + reason)
+})
+
+client.on('message', message => {
+  console.log(message.message)
+})
+
+process.on('SIGINT', () => {
+  client.Disconnect()
+    .then(() => process.exit(0))
+    .catch((e) => {
+      console.log(e)
+      process.exit(1)
+    })
+})
